bd48188c04f7f93b4083ac9e50eb36c81a8ea0e6
TimurDenisenko-Additional <120181244+TimurDenisenko@users.noreply.github.com>
2024-10-15 14:47:48 +0300

    Initial commit

---
+++ Controllers/ToodeController.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Veeb.Models;
+
+namespace Veeb.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static List<Toode> toodeDB = [new(0, "Koola", 1.5, true), new(1, "Jäätis", 5, true)];
+        private static readonly List<Toode> backup = [];
+
+        private static void DeepCopy(List<Toode> from, List<Toode> to)
+        {
+            if (from.SequenceEqual(to))
+                return;
+            if (to.Count > 0)
+                to.Clear();
+            from.ForEach(x =>
+            {
+                to.Add(new Toode(x.Id, x.Name, x.Price, x.IsActive));
+            });
+        }
+        private static void CreateBackup() => DeepCopy(toodeDB, backup);
+        private static void Reorder()
+        {
+            for (int i = 0; i < toodeDB.Count; i++)
+            {
+                toodeDB[i].Id = i;
+            }
+        }
+
+        // GET: toode
+        [HttpGet]
+        public List<Toode> GetTooded() => toodeDB;
+
+        // GET: toode/id
+        [HttpGet("{id}")]
+        public Toode GetToode(int id) => toodeDB.ElementAtOrDefault(id) ?? new Toode();
+
+        // GET: toode/suurenda-hinda/id
+        [HttpPatch("suurenda-hinda/{id}")]
+        public Toode SuurendaHinda(int id)
+        {
+            CreateBackup();
+            Toode toode = toodeDB.ElementAtOrDefault(id) ?? new Toode();
+            if (toode.Id == -1)
+                return toode;
+            toode.Price++;
+            return toode;
+        }
+
+        // GET: toode/change-active/id
+        [HttpPatch("change-active/{id}")]
+        public Toode ChangeActive(int id)
+        {
+            CreateBackup();
+            Toode toode = toodeDB.ElementAtOrDefault(id) ?? new Toode();
+            if (toode.Id == -1)
+                return toode;
+            toode.IsActive = !toode.IsActive;
+            return toode;
+        }
+
+        // GET: toode/change-name/id
+        [HttpPatch("change-name/{id}/{newName}")]
+        public Toode ChangeName(int id, string newName)
+        {
+            CreateBackup();
+            Toode toode = toodeDB.ElementAtOrDefault(id) ?? new Toode();
+            if (toode.Id == -1)
+                return toode;
+            toode.Name = newName;
+            return toode;
+        }
+
+        // GET: toode/multiply-price/id/factor
+        [HttpPatch("multiply-price/{id}/{factor}")]
+        public Toode MultiplyPrice(int id, int factor)
+        {
+            CreateBackup();
+            Toode toode = toodeDB.ElementAtOrDefault(id) ?? new Toode();
+            if (toode.Id == -1)
+                return toode;
+            toode.Price *= factor;
+            return toode;
+        }
+
+        // GET: toode/delete/id
+        [HttpDelete("delete/{id}")]
+        public List<Toode> Delete(int id)
+        {
+            CreateBackup();
+            Toode toode = toodeDB.ElementAtOrDefault(id) ?? new Toode();
+            if (toode.Id == -1)
+                return [];
+            toodeDB.RemoveAt(id);
+            Reorder();
+            return toodeDB;
+        }
+
+        // GET: toode/create/name/price/state
+        [HttpPost("create/{name}/{price}/{state}")]
+        public List<Toode> Create(string name, double price, bool state)
+        {
+            CreateBackup();
+            toodeDB.Add(new Toode(toodeDB.Count, name, price, state));
+            Reorder();
+            return toodeDB;
+        }
+
+        //Another way to add an element
+        // GET: toode/create?id=1&nimi=Koola&hind=1.5&aktiivne=true
+        //[HttpGet("create")] 
+        //public List<Toode> Create([FromQuery] int id, [FromQuery] string nimi, [FromQuery] double hind, [FromQuery] bool aktiivne)
+        //{
+        //    CreateBackup();
+        //    toodeDB.Add(new Toode(id, nimi, hind, aktiivne));
+        //    return toodeDB;
+        //}
+
+        // GET: toode/rate/1.5
+        [HttpPatch("rate/{rate}")]
+        public List<Toode> ChangeRate(double rate)
+        {
+            CreateBackup();
+            toodeDB = toodeDB.Select(x =>
+            {
+                x.Price *= rate;
+                return x;
+            }).ToList();
+            return toodeDB;
+        }
+
+        // GET: toode/clear
+        [HttpDelete("clear")]
+        public string ClearTable()
+        {
+            CreateBackup();
+            toodeDB.Clear();
+            return "The table has been cleared";
+        }
+
+        // GET: toode/state-manage/true
+        [HttpPatch("state-manage")]
+        public string StateManage(bool state)
+        {
+            CreateBackup();
+            toodeDB = toodeDB.Select(x =>
+            {
+                x.IsActive = state;
+                return x;
+            }).ToList();
+            return $"All items have been {(state ? "enabled" : "disabled")}";
+        }
+
+        // GET: toode/backup
+        [HttpPost("backup")]
+        public List<Toode> Backup()
+        {
+            if (backup.Count > 0)
+                DeepCopy(backup, toodeDB);
+            return toodeDB;
+        }
+
+        //GET: toode/max-price
+        [HttpPatch("max-price")]
+        public Toode MaxPrice()
+        {
+            Toode toode = new();
+            foreach (Toode item in toodeDB)
+            {
+                if (item.Price > toode.Price)
+                    toode = item;
+            }
+            return toode;
+        }
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tooted, setTooted] = useState([]);
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const isActiveRef = useRef();
+
+
+  useEffect(() => {
+    fetch("http://localhost:5139/toode")
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }, []);
+
+  function kustuta(index) {
+    fetch("http://localhost:5139/toode/delete/" + index, {"method": "DELETE"})
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+
+  ////////////////////////
+  function lisa() {
+    const uusToode = {
+      "name": nameRef.current.value,
+      "price": Number(priceRef.current.value),
+      "isActive": isActiveRef.current.checked
+    }
+    fetch(`http://localhost:5139/toode/create/
+        ${nameRef.current.value}/${Number(priceRef.current.value)}/${isActiveRef.current.checked}`, {"method": "POST"})
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+  ////////////////////////
+
+  function dollariteks() {
+    const kurss = 1.1;
+    fetch("http://localhost:5139/toode/rate/" + kurss, {"method": "PATCH"})
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="form-section">
+          <label>Nimi</label>
+          <input ref={nameRef} type="text" /> <br />
+          <label>Hind</label>
+          <input ref={priceRef} type="number" /> <br />
+          <label>Aktiivne</label>
+          <input ref={isActiveRef} type="checkbox" /> <br />
+          <button onClick={() => lisa()}>Lisa</button>
+        </div>
+        <div className="list-section">
+          <table>
+            <tr>
+              <th>Nimi</th>
+              <th>Hind</th>
+              <th>Tegevus</th>
+            </tr>
+            {tooted.map((toode, index) => (
+              <tr key={index}>
+                <td>{toode.name}</td>
+                <td>{toode.price}</td>
+                <td>
+                  <button onClick={() => kustuta(index)}>Kustuta</button>
+                </td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      </div>
+      <div className='tools'>
+          <button onClick={() => dollariteks()}>Muuda dollariteks</button>
+      </div>
+  </div>
+  );
+}
+
+export default App;
